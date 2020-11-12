2a150269c13a6d057415344ed0875d693429a6f3
Joe H <joe@joeth.uk>
2020-11-12 12:41:10 +0000

    Start work on check_in function

--- main.c
+++ main.c
@@ -1,4 +1,44 @@
+#include <time.h>
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <limits.h>
+
+char user_info[6][6][20];
+
+void check_in(){
+    srand(time(NULL)); // seed rng
+
+    // init variables
+    char user_id[20];
+    int rand_num = (rand() % 30 + 1);
+    char rand_num_str[2];
+
+    sprintf(rand_num_str, "%d", rand_num);   // make the number into string using sprintf function
+    
+    printf("%s",rand_num_str);
+
+    // Ask user for inputs
+    printf("What is your surname? : ");
+    scanf("%s", &user_id);
+
+    printf("How many guests are in your group? : ");
+    scanf("%s", &user_info[1]);
+    int guests = strtonum(user_info[1]);
+    
+    // validate number of guests
+    switch(guests) {
+
+    }
+
+    printf("Which board type? (FB for full board, HB for half board, BB for bed and breakfast) : ");
+    scanf("%s", &user_info[1]);
+
+    strcat(user_id, rand_num_str); // concatenates surname and the random number into "user_id"
+
+    printf("Your Booking ID is: %s\n",user_id); // debug printf
+
+}
 
 int main() { 
     int userMenuChoice = 0;
@@ -6,11 +46,12 @@ int main() {
 
     while (mainMenuCount == 0){  //Main menu will loop while the count remains unchanged
         printf("Welcome to the Kashyyyk Hotel\n"); //Greeting
-        printf("Would you like to Check In(I), Book a Dinner Table(B), Check Out(O), or Quit(Q)?  ");//Gives the user 4 options
+        printf("Would you like to Check In(I), Book a Dinner Table(B), Check Out(O), or Quit(Q)?  ");  //Gives the user 4 options
         scanf(" %c", &userMenuChoice);  //Reads in the user input and assigns it
         switch (userMenuChoice) {  //Runs a particular part of the code depending on what the user input is
             case 'I':  //When "Check in" option is chosen
                 printf("I\n");  //Placeholder for the "Check In" subroutine
+                check_in();
                 break;
             case 'B':  //When "Book a Dinner Table" option is chosen
                 printf("B\n");  //Placeholder for the "Book a Table" subroutine
